7c4b53418566937946a3aa116366753aee60a8b3
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-10-24 02:45:14 +0300

    fix short info component
    add year

--- src/app/components/short-info/short-info.component.ts
+++ src/app/components/short-info/short-info.component.ts
@@ -1,5 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { LoanService } from 'src/app/loan.service';
+// @Component({
+//   selector: 'app-short-info',
+//   templateUrl: './short-info.component.html',
+//   styleUrls: ['./short-info.component.css'],
+// })
+// export class ShortInfoComponent {
+//   selectedMonth: string = '01'; // по умолчанию январь
+//   metricsCalculated: boolean = false;
+
+//   averageLoanAmount: string = '0.00'; // значение средней суммы округлено до двух знаков после запятой
+//   totalLoanAmount: number = 0;
+//   totalInterestAmount: number = 0;
+//   totalIssuedLoansCount: number = 0;
+//   totalReturnedLoansCount: number = 0;
+
+//   constructor(private loanService: LoanService) {}
+
+//   calculateMetrics() {
+//     this.loanService.getLoans().subscribe((loans) => {
+//       const selectedMonth = this.selectedMonth;
+//       // фильтрация кредитов избранного месяца
+//       const loansForSelectedMonth = loans.filter(
+//         (loan) =>
+//           loan.issuance_date &&
+//           loan.issuance_date.startsWith(`2021-${selectedMonth}`)
+//       );
+
+//       this.averageLoanAmount = this.calculateAverageLoanAmount(
+//         loansForSelectedMonth
+//       ).toFixed(1);
+//       this.totalLoanAmount = this.calculateTotalLoanAmount(
+//         loansForSelectedMonth
+//       );
+//       this.totalInterestAmount = this.calculateTotalInterestAmount(
+//         loansForSelectedMonth
+//       );
+//       this.totalIssuedLoansCount = this.calculateIssuedLoansCount(
+//         loansForSelectedMonth
+//       );
+//       this.totalReturnedLoansCount = this.calculateReturnedLoansCount(
+//         loansForSelectedMonth
+//       );
+
+//       this.metricsCalculated = true;
+//     });
+//   }
+
+//   calculateAverageLoanAmount(loans: any[]): number {
+//     const totalAmount = loans.reduce((sum, loan) => sum + loan.body, 0);
+//     return totalAmount / loans.length;
+//   }
+
+//   calculateTotalLoanAmount(loans: any[]): number {
+//     return loans.reduce((sum, loan) => sum + loan.body, 0);
+//   }
+
+//   calculateTotalInterestAmount(loans: any[]): number {
+//     return loans.reduce((sum, loan) => sum + loan.percent, 0);
+//   }
+
+//   calculateIssuedLoansCount(loans: any[]): number {
+//     return loans.length;
+//   }
+
+//   calculateReturnedLoansCount(loans: any[]): number {
+//     return loans.filter((loan) => loan.actual_return_date).length;
+//   }
+// }
+import { Component } from '@angular/core';
 import { LoanService } from 'src/app/loan.service';
+
 @Component({
   selector: 'app-short-info',
   templateUrl: './short-info.component.html',
@@ -7,6 +78,7 @@ import { LoanService } from 'src/app/loan.service';
 })
 export class ShortInfoComponent {
   selectedMonth: string = '01'; // по умолчанию январь
+  selectedYear: string = '2021'; // по умолчанию 2021 год
   metricsCalculated: boolean = false;
 
   averageLoanAmount: string = '0.00'; // значение средней суммы округлено до двух знаков после запятой
@@ -18,18 +90,23 @@ export class ShortInfoComponent {
   constructor(private loanService: LoanService) {}
 
   calculateMetrics() {
+    const selectedMonth = this.selectedMonth;
+    const selectedYear = this.selectedYear;
+
     this.loanService.getLoans().subscribe((loans) => {
-      const selectedMonth = this.selectedMonth;
-      // фильтрация кредитов избранного месяца
-      const loansForSelectedMonth = loans.filter(
-        (loan) =>
-          loan.issuance_date &&
-          loan.issuance_date.startsWith(`2021-${selectedMonth}`)
-      );
+      // фильтрация кредитов по выбранному месяцу и году
+      const loansForSelectedMonth = loans.filter((loan) => {
+        const issuanceDate = loan.issuance_date;
+        if (issuanceDate) {
+          const [year, month] = issuanceDate.split('-');
+          return year === selectedYear && month === selectedMonth;
+        }
+        return false;
+      });
 
       this.averageLoanAmount = this.calculateAverageLoanAmount(
         loansForSelectedMonth
-      ).toFixed(1);
+      ).toFixed(2);
       this.totalLoanAmount = this.calculateTotalLoanAmount(
         loansForSelectedMonth
       );
@@ -48,6 +125,9 @@ export class ShortInfoComponent {
   }
 
   calculateAverageLoanAmount(loans: any[]): number {
+    if (loans.length === 0) {
+      return 0;
+    }
     const totalAmount = loans.reduce((sum, loan) => sum + loan.body, 0);
     return totalAmount / loans.length;
   }
