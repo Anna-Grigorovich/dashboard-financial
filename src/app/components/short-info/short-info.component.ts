53e63c159857d6645ead6cf143831ffc0c9ca082
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-10-24 17:10:09 +0300

    added interface

--- src/app/components/short-info/short-info.component.ts
+++ src/app/components/short-info/short-info.component.ts
@@ -1,75 +1,5 @@
-// import { Component, OnInit } from '@angular/core';
-// import { LoanService } from 'src/app/loan.service';
-// @Component({
-//   selector: 'app-short-info',
-//   templateUrl: './short-info.component.html',
-//   styleUrls: ['./short-info.component.css'],
-// })
-// export class ShortInfoComponent {
-//   selectedMonth: string = '01'; // по умолчанию январь
-//   metricsCalculated: boolean = false;
-
-//   averageLoanAmount: string = '0.00'; // значение средней суммы округлено до двух знаков после запятой
-//   totalLoanAmount: number = 0;
-//   totalInterestAmount: number = 0;
-//   totalIssuedLoansCount: number = 0;
-//   totalReturnedLoansCount: number = 0;
-
-//   constructor(private loanService: LoanService) {}
-
-//   calculateMetrics() {
-//     this.loanService.getLoans().subscribe((loans) => {
-//       const selectedMonth = this.selectedMonth;
-//       // фильтрация кредитов избранного месяца
-//       const loansForSelectedMonth = loans.filter(
-//         (loan) =>
-//           loan.issuance_date &&
-//           loan.issuance_date.startsWith(`2021-${selectedMonth}`)
-//       );
-
-//       this.averageLoanAmount = this.calculateAverageLoanAmount(
-//         loansForSelectedMonth
-//       ).toFixed(1);
-//       this.totalLoanAmount = this.calculateTotalLoanAmount(
-//         loansForSelectedMonth
-//       );
-//       this.totalInterestAmount = this.calculateTotalInterestAmount(
-//         loansForSelectedMonth
-//       );
-//       this.totalIssuedLoansCount = this.calculateIssuedLoansCount(
-//         loansForSelectedMonth
-//       );
-//       this.totalReturnedLoansCount = this.calculateReturnedLoansCount(
-//         loansForSelectedMonth
-//       );
-
-//       this.metricsCalculated = true;
-//     });
-//   }
-
-//   calculateAverageLoanAmount(loans: any[]): number {
-//     const totalAmount = loans.reduce((sum, loan) => sum + loan.body, 0);
-//     return totalAmount / loans.length;
-//   }
-
-//   calculateTotalLoanAmount(loans: any[]): number {
-//     return loans.reduce((sum, loan) => sum + loan.body, 0);
-//   }
-
-//   calculateTotalInterestAmount(loans: any[]): number {
-//     return loans.reduce((sum, loan) => sum + loan.percent, 0);
-//   }
-
-//   calculateIssuedLoansCount(loans: any[]): number {
-//     return loans.length;
-//   }
-
-//   calculateReturnedLoansCount(loans: any[]): number {
-//     return loans.filter((loan) => loan.actual_return_date).length;
-//   }
-// }
 import { Component } from '@angular/core';
-import { LoanService } from 'src/app/loan.service';
+import { IapiData, LoanService } from 'src/app/loan.service';
 
 @Component({
   selector: 'app-short-info',
@@ -124,7 +54,7 @@ export class ShortInfoComponent {
     });
   }
 
-  calculateAverageLoanAmount(loans: any[]): number {
+  calculateAverageLoanAmount(loans: IapiData[]): number {
     if (loans.length === 0) {
       return 0;
     }
@@ -132,19 +62,19 @@ export class ShortInfoComponent {
     return totalAmount / loans.length;
   }
 
-  calculateTotalLoanAmount(loans: any[]): number {
+  calculateTotalLoanAmount(loans: IapiData[]): number {
     return loans.reduce((sum, loan) => sum + loan.body, 0);
   }
 
-  calculateTotalInterestAmount(loans: any[]): number {
+  calculateTotalInterestAmount(loans: IapiData[]): number {
     return loans.reduce((sum, loan) => sum + loan.percent, 0);
   }
 
-  calculateIssuedLoansCount(loans: any[]): number {
+  calculateIssuedLoansCount(loans: IapiData[]): number {
     return loans.length;
   }
 
-  calculateReturnedLoansCount(loans: any[]): number {
+  calculateReturnedLoansCount(loans: IapiData[]): number {
     return loans.filter((loan) => loan.actual_return_date).length;
   }
 }
--- src/app/loan.service.ts
+++ src/app/loan.service.ts
@@ -11,7 +11,26 @@ export class LoanService {
 
   constructor(private http: HttpClient) {}
 
-  getLoans(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getLoans(): Observable<IapiData[]> {
+    return this.http.get<IapiData[]>(this.apiUrl);
   }
 }
+export interface IapiData {
+  id: number;
+  user: string;
+  issuance_date: string;
+  return_date: string;
+  actual_return_date: string | null;
+  body: number;
+  percent: number;
+}
+
+// {
+//     "id": 1,
+//     "user": "pageantrylamentable",
+//     "issuance_date": "2020-01-11",
+//     "return_date": "2020-01-25",
+//     "actual_return_date": "2021-04-23",
+//     "body": 4500,
+//     "percent": 32535.0
+//   },
